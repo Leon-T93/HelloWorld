1793c0e35b44d82988407aa335c2d213fe7cfc0e
Leon-T93 <leon.thur93@gmail.com>
2024-10-07 19:01:50 +0200

    provjera commita

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Leon Thur");
         System.out.println("Treci sat predavanja");
+        System.out.println("Cetvrti sat predavanja");
     }
 }
