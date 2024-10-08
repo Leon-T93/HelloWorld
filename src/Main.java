9f83c37fb7830044f534356b596f90e058f242aa
Leon-T93 <leon.thur93@gmail.com>
2024-10-08 19:47:11 +0200

    Matematicka funkcija zbrajanja

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Leon Thur");
-        System.out.println("Treci sat predavanja");
-        System.out.println("Cetvrti sat predavanja");
-        System.out.println("Peti sat predavanja");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Molim vas unesite jedan broj:");
+
+        int uneseniBroj = scanner.nextInt();
+
+        System.out.println("Molim vas unesite drugi broj:");
+
+        int uneseniBroj2 = scanner.nextInt();
+
+        int zbroj = uneseniBroj + uneseniBroj2;
+
+        System.out.println("Rezultat : " + zbroj);
     }
 }
