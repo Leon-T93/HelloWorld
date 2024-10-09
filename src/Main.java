2ea28ce578cc3581cc88ce01582cd0ce1158f9f0
Leon-T93 <leon.thur93@gmail.com>
2024-10-09 22:00:06 +0200

    Zadaca sri 9.10.2024.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Molim vas unesite jedan broj:");
-
+        System.out.println("Molim vas unesite jedan broj od 1 do 4:");
         int uneseniBroj = scanner.nextInt();
 
-        System.out.println("Molim vas unesite drugi broj:");
-
-        int uneseniBroj2 = scanner.nextInt();
-
-        int zbroj = uneseniBroj + uneseniBroj2;
-
-        System.out.println("Rezultat : " + zbroj);
+        if (uneseniBroj == 1){
+            System.out.println("Odabrali ste zbrajanje");
+        } else if (uneseniBroj == 2) {
+            System.out.println("Odabrali ste oduzimanje");
+        } else if (uneseniBroj == 3) {
+            System.out.println("Odabrali ste mnozenje");
+        } else if (uneseniBroj == 4) {
+            System.out.println("Odabrali ste dijeljenje");
+        } else {
+            System.out.println("Niste unjeli broj od 1 do 4");
+        }
     }
 }
