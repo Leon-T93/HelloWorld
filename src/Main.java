34955810b5e906a778d42010e3fbb4160cfca456
Leon-T93 <leon.thur93@gmail.com>
2024-10-10 20:24:45 +0200

    zamjena unosa sa sretnim brojem

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Molim vas unesite jedan broj od 1 do 4:");
-        int uneseniBroj = scanner.nextInt();
 
-        if (uneseniBroj == 1){
-            System.out.println("Odabrali ste zbrajanje");
-        } else if (uneseniBroj == 2) {
-            System.out.println("Odabrali ste oduzimanje");
-        } else if (uneseniBroj == 3) {
-            System.out.println("Odabrali ste mnozenje");
-        } else if (uneseniBroj == 4) {
-            System.out.println("Odabrali ste dijeljenje");
-        } else {
-            System.out.println("Niste unjeli broj od 1 do 4");
-        }
+
+
+        int sretniBroj;
+
+        do {
+            System.out.println("Odaberite broj od 1 do 10.");
+
+            sretniBroj = scanner.nextInt();
+
+            if (sretniBroj >7 || sretniBroj <7){
+                System.out.println("Nažalost niste pogodili, probajte ponovo.");
+            }
+
+        }while (sretniBroj >7 || sretniBroj <7);
+
+        System.out.println("Bravo, pogodili ste broj i osvojili....ovaj tekst....");
+
+
+
+
     }
 }
