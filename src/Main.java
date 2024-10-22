264648529d83f0a34081eff7b7da23141fe356eb
Leon-T93 <leon.thur93@gmail.com>
2024-10-23 01:37:59 +0200

    Zadatak 1

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+// Traziti od korisnika unos liste stringova sve dok ne napise Stop, nakon toga izbrojiti koliko stringova sadrzi rijec Java
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Unesite velicinu polja: ");
-        int velicinaPolja = scanner.nextInt();
+        List<Integer> listaBrojeva = new ArrayList<>();
 
-        int[] polje = new int[velicinaPolja];
+        int uneseniBrojevi;
 
-        for (int i = 0; i < polje.length; i++){
-            System.out.println("Unesite vrijednost "+ (i + 1) + ". elementa:");
-            polje[i] = scanner.nextInt();
+        System.out.println("Unesite 10 brojeva.");
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("Unesite broj u listu: ");
+            uneseniBrojevi= scanner.nextInt();
+            listaBrojeva.add(uneseniBrojevi);
         }
 
-        int maxElement = polje[0];
-        for (int i = 1; i<polje.length; i++){
-            if (polje[i] > maxElement){
-                maxElement = polje[i];
+
+        System.out.println("Unjeli ste 10 brojeva.");
+
+
+        int najveciBroj=0;
+        int zbiljaBroj=0;
+        for (int i = 0; i < listaBrojeva.size(); i++)
+            if (najveciBroj< listaBrojeva.get(i)){
+                najveciBroj++;
+            }else if (najveciBroj>= listaBrojeva.get(i)){
+                najveciBroj= zbiljaBroj;
+                System.out.println("Najveci broj: "+ zbiljaBroj);
             }
-        }
 
-        System.out.println("maximalni element polja je: " + maxElement);
+
+
+
 
 
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
