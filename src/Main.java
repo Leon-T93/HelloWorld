08f982950d0e8a1d240f446fae87d191ddd1b0b3
Leon-T93 <leon.thur93@gmail.com>
2024-10-23 01:39:36 +0200

    Zadatak 2

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,14 @@ public class Main {
 
         List<Integer> listaBrojeva = new ArrayList<>();
 
+
         int uneseniBrojevi;
 
         System.out.println("Unesite 10 brojeva.");
 
         for (int i = 0; i < 10; i++){
             System.out.println("Unesite broj u listu: ");
-            uneseniBrojevi= scanner.nextInt();
+            uneseniBrojevi = scanner.nextInt();
             listaBrojeva.add(uneseniBrojevi);
         }
 
@@ -23,15 +24,41 @@ public class Main {
         System.out.println("Unjeli ste 10 brojeva.");
 
 
-        int najveciBroj=0;
-        int zbiljaBroj=0;
-        for (int i = 0; i < listaBrojeva.size(); i++)
-            if (najveciBroj< listaBrojeva.get(i)){
-                najveciBroj++;
-            }else if (najveciBroj>= listaBrojeva.get(i)){
-                najveciBroj= zbiljaBroj;
-                System.out.println("Najveci broj: "+ zbiljaBroj);
-            }
+        int brojac1;
+        int brojac2;
+        int brojac3;
+        int brojac4;
+        int brojac5;
+        int brojac6;
+        int brojac7;
+        int brojac8;
+        int brojac9;
+        int brojac10;
+
+        switch  (listaBrojeva.get()){
+            case "1"
+                brojac1++;
+            case "2"
+                brojac2++;
+            case "3"
+                brojac3++;
+            case "4"
+                brojac4++;
+            case "5"
+                brojac5++;
+            case "6"
+                brojac6++;
+            case "7"
+                brojac7++;
+            case "8"
+                brojac8++;
+            case "9"
+                brojac9++;
+            case "10"
+                brojac10++;
+
+
+        }
 
 
 
