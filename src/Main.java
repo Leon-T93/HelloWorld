2eb82b1241844b9fca35c851b5e79b61ed0c7662
Leon-T93 <leon.thur93@gmail.com>
2024-10-23 20:52:38 +0200

    vjezba 23.10.2024.

--- src/Main.java
+++ src/Main.java
@@ -4,65 +4,30 @@ import java.util.Scanner;
 
 // Traziti od korisnika unos liste stringova sve dok ne napise Stop, nakon toga izbrojiti koliko stringova sadrzi rijec Java
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        List<Integer> brojevi = new ArrayList<>();
 
-        List<Integer> listaBrojeva = new ArrayList<>();
+        System.out.println("Unesite broj elemenata: ");
+        int brojElemenata = scanner.nextInt();
 
-
-        int uneseniBrojevi;
-
-        System.out.println("Unesite 10 brojeva.");
-
-        for (int i = 0; i < 10; i++){
-            System.out.println("Unesite broj u listu: ");
-            uneseniBrojevi = scanner.nextInt();
-            listaBrojeva.add(uneseniBrojevi);
+        for(int i = 0; i< brojElemenata; i++){
+            System.out.println("Unesite željeni broj: ");
+            brojevi.add(scanner.nextInt());
         }
 
+        int trenutniBroj=brojevi.get(0);
 
-        System.out.println("Unjeli ste 10 brojeva.");
+        for (int i=0; i< brojevi.size();i++){
+            if(brojevi.get(i)==trenutniBroj){
+                trenutniBroj=brojevi.get(i);
+                System.out.println("True");
 
-
-        int brojac1;
-        int brojac2;
-        int brojac3;
-        int brojac4;
-        int brojac5;
-        int brojac6;
-        int brojac7;
-        int brojac8;
-        int brojac9;
-        int brojac10;
-
-        switch  (listaBrojeva.get()){
-            case "1"
-                brojac1++;
-            case "2"
-                brojac2++;
-            case "3"
-                brojac3++;
-            case "4"
-                brojac4++;
-            case "5"
-                brojac5++;
-            case "6"
-                brojac6++;
-            case "7"
-                brojac7++;
-            case "8"
-                brojac8++;
-            case "9"
-                brojac9++;
-            case "10"
-                brojac10++;
-
+            }
 
         }
-
-
-
-
 
 
 
