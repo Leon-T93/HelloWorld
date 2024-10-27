5471e647d12fa4b2d28c296dce3fed5f30cd4d42
Leon-T93 <leon.thur93@gmail.com>
2024-10-28 00:46:38 +0100

    zadaca 2

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,44 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        List<String> rijeci = new ArrayList<>();
-
-        System.out.println("Unesite recenicu: ");
-        String recenica = scanner.nextLine();
+        List<String> imena = new ArrayList<>();
+        List<Integer> brojevi = new ArrayList<>();
 
 
-        String [] rijeciRecenice = recenica.split(" ");
+        System.out.println("Odaberite broj za odredenu radnju:");
+        System.out.println("1 - Dodaj kontakt");
+        System.out.println("2 - Pretrazi po imenu");
+        System.out.println("3 - Pretrazi po broju");
+        System.out.println("4 - Ispisi sve");
+        System.out.println("5 - Izlaz");
 
-        int brojrijeci = rijeciRecenice.length;
+        int odabir = scanner.nextInt();
 
-        System.out.println("dadada: " + brojrijeci);
+        switch (odabir){
+            case 1:
+                System.out.println("Unesite ime: ");
+                String unesenoIme = scanner.nextLine();
+                imena.add(unesenoIme);
 
+                System.out.println("Unesite broj: ");
+                int broj = scanner.nextInt();
+                brojevi.add(broj);
 
+                break;
+            case 2:
 
+                break;
+            case 3:
 
+                break;
+            case 4:
 
+                break;
+            case 5:
 
+                break;
 
+        }
 
 
 
