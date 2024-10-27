27a506878c865594b8796a6f550d2a87db6a291f
Leon-T93 <leon.thur93@gmail.com>
2024-10-28 00:44:49 +0100

    zadaca 1

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-//Tražiti od korisnika da unese broj elemenata u listi te popuni elemente liste.
-//Nakon toga pretražiti po listi da li postoji ijedan duplikat, ukoliko postoji ispisi vrijednost true, ukoliko ne postoji ispisi vrijednost false.
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        List<Integer> brojevi = new ArrayList<>();
+        List<String> rijeci = new ArrayList<>();
 
-        System.out.println("Unesite broj elemenata: ");
-        int brojElemenata = scanner.nextInt();
+        System.out.println("Unesite recenicu: ");
+        String recenica = scanner.nextLine();
 
-        for (int i = 0; i < brojElemenata; i++) {
-            System.out.println("Unesite željeni broj: ");
-            brojevi.add(scanner.nextInt());
-        }
 
-        provjeraSortiranosti(brojevi);
-    }
+        String [] rijeciRecenice = recenica.split(" ");
 
-    public static Boolean provjeraSortiranosti(List<Integer> lista){
+        int brojrijeci = rijeciRecenice.length;
 
-        Boolean sortirano = true;
+        System.out.println("dadada: " + brojrijeci);
 
-        for (int i=0; i<lista.size()-1; i++){
-            if (lista.get(i)>lista.get(i+1)){
-                System.out.println("Lista nije uzlazna!");
-                sortirano = false;
-                break;
-            }
-        }
-        if (sortirano == true){
-            System.out.println("Lista je uzlazna");
-        }
 
-        return sortirano;
+
+
+
+
+
+
+
 
     }
+
+
 }
 
 
