aadf77c9d7234209e038c44dcbb1b0e2bd8d2a76
Leon-T93 <leon.thur93@gmail.com>
2024-11-03 20:20:51 +0100

    Zadaca 30.10.2024.

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Unesite iznos koji trebate dobiti: ");
-        int unos= scanner.nextInt();
-        System.out.println("Unjeli ste: " + unos);
-
-        int[] novcanice = {500,100,50,20,10,5,2,1};
+        Polaznik Marko = new Polaznik("Marko", "Markic");
+        Polaznik Ivan = new Polaznik("Ivan", "Ivic", 25, "M");
+        Polaznik TkoJeTajPokremon = new Polaznik();
 
 
+        System.out.println(Marko.dohvatiIme() +" "+ Marko.dohvatiPrezime());
 
-        for(int i = 0; i< novcanice.length && unos!=0; i++){
-            if(unos>= novcanice[i]){
-                    System.out.println("Novcanica "+ novcanice[i]+" :"+unos/ novcanice[i]);
-                unos=unos% novcanice[i];
-            }
-        }
+        Ivan.promjeniDob(26);
 
+        TkoJeTajPokremon.dodajIme("Pikachu");
+        TkoJeTajPokremon.dodajPrezime("Strujic");
 
-
+        
 
 
 
 
 
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
