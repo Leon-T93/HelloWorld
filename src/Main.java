48e0e9c44335e65a09746adb74d957e2a580b5ec
Leon-T93 <leon.thur93@gmail.com>
2024-11-13 21:09:22 +0100

    Zadatak faktorijel

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,56 @@ import java.math.RoundingMode;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 
-        Scanner scanner = new Scanner(System.in);
 
+
+
+        //●Obradi iznimke koje se mogu pojaviti tijekom računanja faktorijela. Na primjer, obradi situaciju kada korisnik unese negativan broj jer faktorijel nije
+        // definiran za negativne brojeve.
+        //●Ispiši rezultat ili poruku o greški, ovisno o tome je li računanje faktorijela uspješno ili ne
+
+
+
+
+        System.out.println("Unesite željeni broj: ");
+        int broj;
+
+        boolean ispravniUnos =false;
+
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                broj = scanner.nextInt();
+                ispravniUnos= true;
+
+
+            } catch (Exception e) {
+                System.out.println("Unjeli ste slovo ili decimalan broj, probajte ponovno!");
+                broj=1;
+
+            }
+
+            if (broj <0) {
+                throw new Exception("Nemoze negativan broj");
+            }
+
+        }while (ispravniUnos!=true);
+
+        int faktorijelBroja = 1;
+
+
+
+        for (int i = 1; i < (broj + 1); i++) {
+        faktorijelBroja = faktorijelBroja * i;
+        }
+
+
+
+
+
+        System.out.println("Faktorijel od broja " + broj + " je " + faktorijelBroja);
 
 
 
