2dcde17e86b85718fe7ec5ab6e1343e0ec794a9e
Leon-T93 <leon.thur93@gmail.com>
2024-11-21 20:48:36 +0100

    Dodavanje Zaposlenika

--- src/Main.java
+++ src/Main.java
@@ -18,37 +18,12 @@ public class Main {
         tvrtka.dodajZaposlenika(zaposlenik);
 
 
+
+
+
+
+
         
-
-
-
-
-
-
-
-
-
-
-
-        Razred razred = new Razred("1B", "Petar");
-
-        Ucenik ucenik1 = new Ucenik("Ana", "Anic", 4);
-        razred.dodajUcenika(ucenik1);
-
-        Ucenik ucenik2 = new Ucenik("Ivan", "Ivic", 3);
-        razred.dodajUcenika(ucenik2);
-
-        Ucenik ucenik3 = new Ucenik("Marko", "Maric", 2);
-        razred.dodajUcenika(ucenik3);
-
-        Ucenik ucenik4 = new Ucenik("Filip", "Filipic", 5);
-        razred.dodajUcenika(ucenik4);
-
-        for (Ucenik ucenik : razred.getUcenici()) {
-            ucenik.ispisiInfo();
-        }
-
-        razred.pronadiNajboljegUcenikaPoProsjekuOcjena();
 
 
 
