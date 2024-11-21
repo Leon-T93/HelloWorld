1dcd9ef5f47fbde0894b13179bc3da5b94dd054d
Leon-T93 <leon.thur93@gmail.com>
2024-11-21 21:39:38 +0100

    Dodavanje zaposlenika

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,52 @@ public class Main {
 
 
 
+            int odabir = 0;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Molim odaberite akciju: ");
+            System.out.println("1 za unos zaposlenika: ");
+            System.out.println("2 za ispis zaposlenika: ");
+            System.out.println("3 za pronalazenje zaposlenika sa najvecom placom: ");
+            System.out.println("4 za izlazak iz menija. ");
+
+            odabir = scanner.nextInt();
 
 
-        Scanner scanner = new Scanner(System.in);
-        Zaposlenik zaposlenik = new Zaposlenik();
-        Tvrtka tvrtka= new Tvrtka("Tcom", null);
-        System.out.println("Unesite ime zaposlenika: ");
-        zaposlenik.setIme(scanner.nextLine());
-        System.out.println("Unesite prezime zaposlenika: ");
-        zaposlenik.setPrezime(scanner.nextLine());
-        System.out.println("Unesite placu zaposlenika: ");
-        zaposlenik.setPlaca(scanner.nextDouble());
+                    Zaposlenik zaposlenik = new Zaposlenik();
+                    Tvrtka tvrtka= new Tvrtka("Tcom", null);
+            switch (odabir) {
+                case 1:
+                    System.out.println("Unesite ime zaposlenika: ");
+                    zaposlenik.setIme(scanner.next());
+                    System.out.println("Unesite prezime zaposlenika: ");
+                    zaposlenik.setPrezime(scanner.next());
+                    System.out.println("Unesite placu zaposlenika: ");
+                    zaposlenik.setPlaca(scanner.nextDouble());
 
-        tvrtka.dodajZaposlenika(zaposlenik);
+                    tvrtka.dodajZaposlenika(zaposlenik);
+
+                    break;
+
+                case 2:
+                    tvrtka.ispisiZaposlenike();
+                    break;
+
+                case 3:
+                    tvrtka.pronadjiNajvecuPlacu();
+                    break;
+
+                default:
+            }
+        }while (odabir != 4);
 
 
 
 
-        tvrtka.ispisiZaposlenike();
 
-        tvrtka.pronadjiNajvecuPlacu();
+
+
+
 
 
 
