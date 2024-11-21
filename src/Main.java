4e512aed1856e4d3842ac9a6ce699cd4b90fd002
Leon-T93 <leon.thur93@gmail.com>
2024-11-21 21:42:28 +0100

    Dodavanje Zaposlenika

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
 
 
+            Zaposlenik zaposlenik = new Zaposlenik();
+            Tvrtka tvrtka= new Tvrtka("Tcom", null);
             int odabir = 0;
         do {
             Scanner scanner = new Scanner(System.in);
@@ -17,8 +19,6 @@ public class Main {
             odabir = scanner.nextInt();
 
 
-                    Zaposlenik zaposlenik = new Zaposlenik();
-                    Tvrtka tvrtka= new Tvrtka("Tcom", null);
             switch (odabir) {
                 case 1:
                     System.out.println("Unesite ime zaposlenika: ");
