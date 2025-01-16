f16971a4369ace726901dd16a7f7023c3ff559ed
Leon-T93 <leon.thur93@gmail.com>
2025-01-16 21:00:40 +0100

    Zadatak-upravljanje osobama

--- src/Main.java
+++ src/Main.java
@@ -1,242 +1,96 @@
 import database.DatabaseService;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
 
         Scanner scanner= new Scanner(System.in);
 
 
-
-        textZaIzbor();
-
-        int odabir;
-
-        odabir = scanner.nextInt();
-
-        while (odabir <5){
-            switch (odabir) {
-                case 1:
-                    unosGrada();
-                    break;
-                case 2:
-                    izmjenaGrada();
-                    break;
-                case 3:
-                    brisanjeGrada();
-                    break;
-                case 4:
-                    prikazSvihGradovaSortiranihPoNazivu();
-                    break;
-            }
-            textZaIzbor();
-            odabir = scanner.nextInt();
-        }
-
-
-    }
-    
-
-    private static void textZaIzbor() {
-        System.out.println("\n" +"\n" +"Odaberite jednu od sljedečih opcija: " + "\n" +
-                "(1) za unos novog grada" + "\n" +
-                "(2) za izmjenu postoječeg grada" + "\n" +
-                "(3) za brisanje postoječeg grada" + "\n" +
-                "(4) za prikaz svih gradova sortiranih po nazivu" + "\n" +
-                "(5) za izlaz");
-    }
-
-
-    private static void unosGrada() {
         Connection connection = DatabaseService.createConnection();
 
-        Scanner scanner= new Scanner(System.in);
 
-        try {
-            String query = "INSERT INTO Grad (Naziv,DrzavaID) VALUES (?,?)";
-            PreparedStatement statement = connection.prepareStatement(query);
+        
 
-            System.out.println("Molim unesite naziv novog grada.");
-
-            String nazivGrada = scanner.next();
-
-            System.out.println("Molim unesite ID države kojoj grad pripada iz sljedečeg popisa: " + "\n");
-
-            popisDrzava();
-
-            Integer drzavaGrada = scanner.nextInt();
-
-            statement.setString(1, nazivGrada);
-            statement.setInt(2, drzavaGrada);
+        obrisiOsobePoDatumuRodenja(connection);
 
 
-            Integer result = statement.executeUpdate();
-
-
-
-            System.out.println("\n" +"Broj promjena: " + result);
-            statement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
+        connection.close();
     }
 
 
-    private static void popisDrzava() {
-        Connection connection = DatabaseService.createConnection();
-
-        try {
-            String query = "SELECT * FROM Drzava";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
 
 
-            while (resultSet.next()) {
-                System.out.println("ID: " + resultSet.getInt("IDDrzava") +" = "+ resultSet.getString("Naziv"));
-            }
+    public static void dodavanjeNovihOsoba(Connection connection) throws SQLException {
+
+        Osoba osoba1 = new Osoba("Marko", "Maric" ,"20010501");
+        Osoba osoba2 = new Osoba("Filip", "Filic" ,"20020602");
+        Osoba osoba3 = new Osoba("Ivan", "Ivic" ,"20030703");
+        Osoba osoba4 = new Osoba("Ana", "Anic" ,"20040804");
+        Osoba osoba5 = new Osoba("Mara", "Maric" ,"20050905");
+
+        List<Osoba> listaOsoba = new ArrayList<>();
+
+        listaOsoba.add(osoba1);
+        listaOsoba.add(osoba2);
+        listaOsoba.add(osoba3);
+        listaOsoba.add(osoba4);
+        listaOsoba.add(osoba5);
 
 
-            resultSet.close();
-            statement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+        String query = "INSERT INTO Osoba (Ime,Prezime, DatumRodenja) VALUES (?,?,?)";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        for (Osoba osoba : listaOsoba) {
+            preparedStatement.setString(1, osoba.getIme());
+            preparedStatement.setString(2, osoba.getPrezime());
+            preparedStatement.setString(3, osoba.getDatum());
+            preparedStatement.executeUpdate();
+            System.out.println("Unjeti podatak: " + osoba.getIme() + " " + osoba.getPrezime() + " "+ osoba.getDatum());
         }
+
+        preparedStatement.close();
     }
 
 
-    private static void izmjenaGrada() {
-        Connection connection = DatabaseService.createConnection();
+    public static void azuriranjePrezimenaPoIDu(Connection connection) throws SQLException {
 
-        Scanner scanner= new Scanner(System.in);
+        String callProcedure = "{CALL AzurirajPrezime(?,?)}";
+        CallableStatement callableStatement = connection.prepareCall(callProcedure);
 
-        try {
-            String query = "UPDATE Grad SET Naziv = ?, DrzavaID = ? WHERE IDGrad = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
+        int idOsobe = 5;
+        String novoPrezime = "Prezimić";
+        callableStatement.setInt(1, idOsobe);
+        callableStatement.setString(2,novoPrezime);
 
-            prikazSvihGradovaSortiranihPoNazivu();
+        callableStatement.execute();
 
-            System.out.println("Molim unesite ID grada kojem želite izmjeniti naziv.");
+        System.out.println("Promjenili ste prezime osobe sa id-em "+ idOsobe + " u " + novoPrezime);
 
-            Integer IDStarogNazivaGrada = scanner.nextInt();
-
-            System.out.println("Molim unesite novi naziv grada.");
-
-            String noviNazivGrada = scanner.next();
-
-            System.out.println("Molim unesite IDDrzave kojoj pripada taj grad. Popis država: ");
-
-            popisDrzava();
-
-            Integer noviIDDrzave = scanner.nextInt();
-
-
-            statement.setString(1, noviNazivGrada);
-            statement.setInt(2, noviIDDrzave);
-            statement.setInt(3, IDStarogNazivaGrada);
-
-            Integer result = statement.executeUpdate();
-
-
-            System.out.println("\n" +"Broj promjena: " + result);
-            statement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
+        callableStatement.close();
     }
 
+    public static void obrisiOsobePoDatumuRodenja(Connection connection) throws SQLException {
 
-    private static void brisanjeGrada() {
-        Connection connection = DatabaseService.createConnection();
+        String callProcedure = "{CALL ObrisiOsobePoDatumuRodenja(?)}";
+        CallableStatement callableStatement = connection.prepareCall(callProcedure);
 
-        Scanner scanner= new Scanner(System.in);
+        String datumDoKojegSeBrisuOsobe = "20030703";
+        callableStatement.setString(1,datumDoKojegSeBrisuOsobe);
 
-        try {
-            String query = "DELETE FROM Grad WHERE IDGrad = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
+        callableStatement.execute();
 
-            prikazSvihGradovaSortiranihPoNazivu();
+        System.out.println("Obrisali ste sve osobe koje su rođene prije ovog datuma: " + datumDoKojegSeBrisuOsobe);
 
-            System.out.println("Molim unesite ID grada koji želite obrisati.");
-
-            Integer IDGradaZaBrisanje = scanner.nextInt();
-
-            statement.setInt(1, IDGradaZaBrisanje);
-
-            Integer result = statement.executeUpdate();
-
-
-            System.out.println("\n" +"Broj promjena: " + result);
-            statement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
+        callableStatement.close();
     }
-
-
-    private static void prikazSvihGradovaSortiranihPoNazivu() {
-        Connection connection = DatabaseService.createConnection();
-
-        try {
-            String query = "SELECT * FROM Grad ORDER BY Naziv";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-
-
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("Naziv")+ " --- " +"IDGrad = " + resultSet.getInt("IDGrad") +" --- "+
-                        "DrzavaID = "+ resultSet.getInt("DrzavaID"));
-
-            }
-
-            resultSet.close();
-            statement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-
 }
+
+
 
