5088c3dfaa86518fd5a41092e717fe8c3529f2b4
Leon-T93 <leon.thur93@gmail.com>
2025-01-20 20:56:35 +0100

    Azuriranje zaliha i generiranje racuna

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,8 @@ public class Main {
 
 
 
-        brisanjeRacunaPoIdRacuna(connection);
 
+        azuriranjeZalihaProizvodaIGeneriranjeRacuna(connection);
 
 
 
@@ -31,26 +31,53 @@ public class Main {
 
 
 
-    private static void brisanjeRacunaPoIdRacuna (Connection connection) throws SQLException {
+    private static void azuriranjeZalihaProizvodaIGeneriranjeRacuna (Connection connection) throws SQLException {
         Scanner scanner= new Scanner(System.in);
 
-        System.out.println("Molim unesite ID Racuna koji zelite obrisati.");
 
 
         connection.setAutoCommit(false);
 
         try {
-            String unesenIDRacuna = scanner.next();
-            String query = "DELETE FROM Stavka WHERE RacunID = ?";
+            String query = "INSERT INTO Racun (DatumIzdavanja,BrojRacuna,KupacID,KomercijalistID,KreditnaKarticaID) VALUES ('20010901', 'Test1',378,279,16281)";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, unesenIDRacuna);
 
-            String query2 = "DELETE FROM Racun WHERE IDRacun =?";
-            PreparedStatement preparedStatement2 = connection.prepareStatement(query2);
-            preparedStatement2.setString(1, unesenIDRacuna);
+            String query2= "SELECT * FROM Racun WHERE BrojRacuna= 'Test1'";
+            PreparedStatement preparedStatement2= connection.prepareStatement(query2);
+            ResultSet resultSet =preparedStatement2.executeQuery();
+            String brojRacunauStavki=resultSet.getString("IDRacun");
+
+
+            String query3= "INSERT INTO Stavka (RacunID,Kolicina,ProizvodID,CijenaPoKomadu,PopustUPostocima,UkupnaCijena) VALUES (?,1,776,2024.994000,0,2024.994000)";
+            PreparedStatement preparedStatement3= connection.prepareStatement(query3);
+            preparedStatement3.setString(1,brojRacunauStavki);
+
+
+            String query4= "SELECT * FROM Proizvod WHERE IDProizvod = 776";
+            PreparedStatement preparedStatement4= connection.prepareStatement(query4);
+            ResultSet resultSet2 =preparedStatement4.executeQuery();
+            Integer minimalnaKolicinaNaSkladistu= resultSet2.getInt("MinimalnaKolicinaNaSkladistu");
+
+
+            PreparedStatement preparedStatement5=null;
+
+            if (minimalnaKolicinaNaSkladistu >0){
+                String query5= "UPDATE Proizvod SET MinimalnaKolicinaNaSkladistu = ? WHERE IDProizvod = 776 ";
+                preparedStatement5= connection.prepareStatement(query5);
+                preparedStatement5.setInt(1,(minimalnaKolicinaNaSkladistu-1));
+
+            }else connection.rollback();
+
+
+
+
+
 
             preparedStatement.executeUpdate();
             preparedStatement2.executeUpdate();
+            preparedStatement3.executeUpdate();
+            preparedStatement4.executeUpdate();
+            preparedStatement5.executeUpdate();
 
             connection.commit();
 
